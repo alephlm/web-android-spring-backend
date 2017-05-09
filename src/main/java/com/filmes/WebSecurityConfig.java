ce308273051238082c93124b24dd4850fcf8d383
Aleph Mota <alephlm@outlook.com>
2017-05-09 02:21:45 -0300

    Ajustado autenticacao para criacao de usuario

--- src/main/java/com/filmes/WebSecurityConfig.java
+++ src/main/java/com/filmes/WebSecurityConfig.java
@@ -23,7 +23,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-    http.httpBasic().and().authorizeRequests().antMatchers("/resources/**", "/registration").permitAll().anyRequest().authenticated().and().csrf().disable();
+    http.httpBasic().and().authorizeRequests().antMatchers("/resources/**", "/user/registration").permitAll().anyRequest().authenticated().and().csrf().disable();
 
     }
 
